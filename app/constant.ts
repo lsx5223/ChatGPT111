d1c2185c6e5839cd7e375ed22e27531059598bb1
lsx5223 <130224103+lsx5223@users.noreply.github.com>
2023-04-08 21:13:27 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "1032902659";
 export const REPO = "ChatGPT-NEW-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
+export const REPO_URL = `https://qm.qq.com/cgi-bin/qm/qr?k=BGdGHQfXBbPTifZPjZA8xC8aOBu2Q1Fs&noverify=0`;
+export const UPDATE_URL = `https://github.com/1032902659/ChatGPT-NEW-Web#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
